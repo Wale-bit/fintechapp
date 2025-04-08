27904d2524435f055593344a1bb36fef3771f6e6
Wale <eniolaolawale2017@gmail.com>
2025-04-08 05:15:40 +0100

    Add main.ts file for NestJS bootstrap

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+// src/main.ts
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fintech API')
+    .setDescription('API for a simple fintech application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+}
+bootstrap();
